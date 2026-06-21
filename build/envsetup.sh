efe1babc076e245d58648f17105f2f7e677423d0
welcomeworld <guduziyousiwang@gmail.com>
2026-06-21 23:26:39 +0800

    fix harmony product soong namespace

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -14,6 +14,8 @@ function check_product()
     fi
     if (echo -n $1 | grep -q -e "^lineage_") ; then
         LINEAGE_BUILD=$(echo -n $1 | sed -e 's/^lineage_//g')
+    elif (echo -n $1 | grep -q -e "^harmony_") ; then
+    	LINEAGE_BUILD=$(echo -n $1 | sed -e 's/^harmony_//g')
     else
         LINEAGE_BUILD=
     fi
